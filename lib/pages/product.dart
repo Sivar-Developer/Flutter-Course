34b22e1d187b597b8bd56792a1d9d41531fce7db
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-10-20 15:04:49 +0300

    6.5 Improving the look of product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -7,10 +7,21 @@ class ProductPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Product'),
       ),
-      body: Column(children: <Widget>[
-        Text('Details!'),
-        RaisedButton(child: Text('Back'), onPressed: () => Navigator.pop(context))
-      ],),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+        Image.asset('assets/food.jpg'),
+        Container(padding: EdgeInsets.all(10.0), child: Text('Details!'),),
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            color: Theme.of(context).primaryColor,
+            textColor: Colors.white,
+            child: Text('Back'),
+            onPressed: () => Navigator.pop(context)
+          )
+      )],),
     );
   }
 }
