66ca2247936761a6bec8d913b83d0932735f71c9
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-10-25 15:07:45 +0300

    8.11 Centering Input Fields

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -27,49 +27,54 @@ class _AuthPageState extends State<AuthPage> {
           )
         ),
         padding: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              }
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              }
-            ),
-            SwitchListTile(
-              value: _accepTerms,
-              onChanged: (bool value) {
-                setState(() {
-                  _accepTerms = value;
-                });
-              },
-              title: Text('Accept Terms'),
-            ),
-            SizedBox(
-              height: 10.0
-            ),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: Text('LOGIN'),
-              onPressed: () {
-                print(_emailValue);
-                print(_passwordValue);
-                Navigator.pushReplacementNamed(context, '/products');
-              },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email', filled: true, fillColor: Colors.white),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (String value) {
+                    setState(() {
+                      _emailValue = value;
+                    });
+                  }
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Password', filled: true, fillColor: Colors.white),
+                  obscureText: true,
+                  onChanged: (String value) {
+                    setState(() {
+                      _passwordValue = value;
+                    });
+                  }
+                ),
+                SwitchListTile(
+                  value: _accepTerms,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _accepTerms = value;
+                    });
+                  },
+                  title: Text('Accept Terms'),
+                ),
+                SizedBox(
+                  height: 10.0
+                ),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  child: Text('LOGIN'),
+                  onPressed: () {
+                    print(_emailValue);
+                    print(_passwordValue);
+                    Navigator.pushReplacementNamed(context, '/products');
+                  },
+                )
+              ],
             )
-          ],
+          )
         )
       )
     );
