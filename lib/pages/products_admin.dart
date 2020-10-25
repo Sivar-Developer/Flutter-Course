b4b2866d2b72f53a2300789f19a15ab0cd1dc47a
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-10-25 15:16:16 +0300

    Using Icon in our sidemenu

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -22,9 +22,10 @@ class ProductsAdminPage extends StatelessWidget {
               title: Text('Choose'),
             ),
             ListTile(
+              leading: Icon(Icons.shop),
               title: Text('All Products'),
               onTap: () {
-                Navigator.pushReplacementNamed(context, '/');
+                Navigator.pushReplacementNamed(context, '/products');
               }
             )
           ],
