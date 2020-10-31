f3d9bc84e2d5f19d349f97c00ea9832088c9da37
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-10-31 21:05:07 +0300

    13.6 Handling error and Loading spinner while signup

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -104,9 +104,24 @@ class _AuthPageState extends State<AuthPage> {
       login(_formData['email'], _formData['password']);
     } else {
       final Map<String, dynamic> successInformation = await signup(_formData['email'], _formData['password']);
-      if(successInformation['success']) Navigator.pushReplacementNamed(context, '/products');
+      print(successInformation['success']);
+      if(successInformation['success']) {
+        Navigator.pushReplacementNamed(context, '/products');
+      } else {
+        showDialog(context: context, builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('An Error Occurred!'),
+            content: Text(successInformation['message']),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Okey'),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ]
+          );
+        });
+      }
     }
-    Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
@@ -147,9 +162,11 @@ class _AuthPageState extends State<AuthPage> {
                     ),
                     SizedBox(height: 10.0),
                     ScopedModelDescendant<MainModel>(builder: (BuildContext context, Widget child, MainModel model) {
-                      return RaisedButton(
+                      return model.isLoading
+                      ? CircularProgressIndicator()
+                      : RaisedButton(
                         textColor: Colors.white,
-                        child: Text('LOGIN'),
+                        child: Text(_authMode == AuthMode.Login ? 'Login' : 'Signup'),
                         onPressed: () => _submitForm(model.login, model.signup),
                       );
                     })
--- lib/scoped-models/user.dart
+++ lib/scoped-models/user.dart
@@ -10,13 +10,26 @@ mixin UserModel on ConnectedProductsModel {
   }
 
   Future<Map<String, dynamic>> signup(String email, String password) async {
+    isLoading = true;
+    notifyListeners();
     final Map<String, dynamic> authData = {
       'email': email,
       'password': password,
       'returnSecureToken': true
     };
     final http.Response response = await http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyClI5xhSl0zHFWIvFU21n78MtjEeZOu7HU', body: json.encode(authData), headers: {'Content-Type': 'application/json'});
-    print(json.decode(response.body));
-    return {'success': true, 'message': 'Authentication Succeeded!'};
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    bool hasError = true;
+    String message = 'Something went wrong';
+    if(responseData.containsKey('idToken')) {
+      hasError = false;
+      message = 'Authentication Succeeded!';
+    } else if(responseData['error']['message'] == 'EMAIL_EXISTS') {
+      message = 'This email is already exists';
+    }
+    isLoading = false;
+    notifyListeners();
+    print({'success': !hasError, 'message': message});
+    return {'success': !hasError, 'message': message};
   }
 }
