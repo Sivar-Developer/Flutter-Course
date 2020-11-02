e8f3281b302dc5e0bd815be83e817b7c5c1c2b0c
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-02 16:46:46 +0300

    14.7 Geocoding API & Map coordinates

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
@@ -14,27 +16,63 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   final FocusNode _addressInputFocusNode = FocusNode();
   final TextEditingController _addressInputController = TextEditingController();
+  dynamic coords = {'lat': 36.19263133660091, 'lng': 44.00578426256253};
+  List<Marker> allMarkers = [];
 
   @override
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
+    allMarkers.add(
+      Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: false,
+        onTap: () {print('Marker Tapped');},
+        position: LatLng(coords['lat'], coords['lng'])
+      )
+    );
     super.initState();
   }
 
   @override
   void dispose() {
     _addressInputFocusNode.removeListener(_updateLocation);
+    allMarkers.add(
+      Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: false,
+        onTap: () {print('Marker Tapped');},
+        position: LatLng(coords['lat'], coords['lng'])
+      )
+    );
     super.dispose();
   }
 
   Future<dynamic> getStaticMap(String address) async {
+    if(address.isNotEmpty) {
     final Uri uri = Uri.https(
       'maps.googleapis.com',
       '/maps/api/geocode/json',
       {'address': address, 'key': 'AIzaSyB4qGTCPJNlRkroSxsoqYm_xnPUruCdK3E'}
     );
     final http.Response response  = await http.get(uri);
-    return;
+    final decodedResponse = json.decode(response.body);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    setState(() {
+      coords = decodedResponse['results'][0]['geometry']['location'];
+    });
+    print(coords);
+    setState(() {
+      allMarkers.add(
+      Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: false,
+        onTap: () {print('Marker Tapped');},
+        position: LatLng(coords['lat'], coords['lng'])
+      )
+    );
+    });
+    return coords;
+    }
   }
 
   void _updateLocation() {
@@ -61,8 +99,9 @@ class _LocationInputState extends State<LocationInput> {
           child: GoogleMap(
             initialCameraPosition: CameraPosition(
               target: LatLng(36.19263133660091, 44.00578426256253),
-              zoom: 15,
+              zoom: 12,
             ),
+            markers: Set.from(allMarkers),
             myLocationEnabled: true,
             mapType: MapType.normal,
             compassEnabled: true,
