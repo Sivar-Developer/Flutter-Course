e9bbc67e4acdbbdacc7a57962281182ad546543d
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-06 20:30:58 +0300

    Dsplay map dialog on product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -3,12 +3,72 @@ import 'package:flutter_course/models/product.dart';
 import 'package:flutter_course/widgets/products/address_tag.dart';
 import 'package:flutter_course/widgets/products/price_tag.dart';
 import 'package:flutter_course/widgets/ui_elements/title_default.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/product.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   final Product product;
 
   ProductPage(this.product);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ProductPageState();
+  }
+}
+class ProductPageState extends State<ProductPage> {
+  List<Marker> allMarkers = [];
+
+  @override
+  void initState() {
+    allMarkers.add(
+      Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: false,
+        onTap: () {print('Marker Tapped');},
+        position: LatLng(widget.product.location.latitude, widget.product.location.longitude)
+    ));
+    super.initState();
+  }
+
+  Future<dynamic> _showMap(BuildContext context) {
+      return showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: MaterialLocalizations.of(context)
+          .modalBarrierDismissLabel,
+      barrierColor: Colors.black45,
+      transitionDuration: const Duration(milliseconds: 200),
+      pageBuilder: (BuildContext buildContext,
+          Animation animation,
+          Animation secondaryAnimation) {
+        return Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width - 10,
+            height: 550,
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Column(
+              children: [
+                Container(
+                  height: 500,
+                  child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(36.19263133660091, 44.00578426256253),
+                    zoom: 12,
+                  ),
+                  markers: Set.from(allMarkers),
+                  myLocationEnabled: true,
+                  mapType: MapType.normal,
+                  compassEnabled: true,
+                ),
+                )
+              ],
+            ),
+          ),
+        );
+      });
+  }
 
   Widget _buildTitlePriceRow(double price, String title) {
     return Container(
@@ -34,22 +94,25 @@ class ProductPage extends StatelessWidget {
       return Future.value(true);
     }, child: Scaffold(
         appBar: AppBar(
-          title: Text(product.title),
+          title: Text(widget.product.title),
         ),
         body: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
           FadeInImage(
-            image: NetworkImage(product.image),
+            image: NetworkImage(widget.product.image),
             placeholder: AssetImage('assets/placeholder.png'),
           ),
-          _buildTitlePriceRow(product.price, product.title),
-          AddressTag(product.location.address),
+          _buildTitlePriceRow(widget.product.price, widget.product.title),
+          GestureDetector(
+            onTap: () => _showMap(context),
+            child: AddressTag(widget.product.location.address),
+          ),
           Container(
             padding: EdgeInsets.all(10.0),
             child: Text(
-              product.description,
+              widget.product.description,
               textAlign: TextAlign.center
             )
           )
