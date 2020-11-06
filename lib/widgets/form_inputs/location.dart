cae6e3c0c921c69c66dbc033e4e7f2e66d99cbb0
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-06 19:15:28 +0300

    14.15 Getting the user location

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -5,6 +5,7 @@ import 'package:flutter_course/models/location_data.dart';
 import 'package:flutter_course/models/product.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart' as geoloc;
 
 import '../helpers/ensure_visible.dart';
 
@@ -30,7 +31,7 @@ class _LocationInputState extends State<LocationInput> {
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
     if(widget.product != null) {
-      getLocationMarker(widget.product.location.address, false);
+      _getLocationMarker(widget.product.location.address, geocode: false);
     }
     super.initState();
   }
@@ -41,7 +42,7 @@ class _LocationInputState extends State<LocationInput> {
     super.dispose();
   }
 
-  Future<dynamic> getLocationMarker(String address, [geocode = true]) async {
+  Future<dynamic> _getLocationMarker(String address, {bool geocode = true, double lat, double lng}) async {
     if(address.isNotEmpty) {
       if(geocode) {
         final Uri uri = Uri.https(
@@ -54,8 +55,10 @@ class _LocationInputState extends State<LocationInput> {
         final formattedAddress = decodedResponse['results'][0]['formatted_address'];
         final coords = decodedResponse['results'][0]['geometry']['location'];
         _locationData = LocationData(address: formattedAddress, latitude: coords['lat'], longitude: coords['lng']);
-      } else {
+      } else if(lat == null && lng == null) {
         _locationData = widget.product.location;
+      } else {
+        _locationData = LocationData(address: address, latitude: lat, longitude: lng);
       }
 
     widget.setLocation(_locationData);
@@ -74,9 +77,27 @@ class _LocationInputState extends State<LocationInput> {
     }
   }
 
+  Future<String> _getAddress(double lat, double lng) async {
+    final uri = Uri.https(
+          'maps.googleapis.com',
+          '/maps/api/geocode/json',
+          {'latlng': '${lat.toString()},${lng.toString()}', 'key': 'AIzaSyB4qGTCPJNlRkroSxsoqYm_xnPUruCdK3E'}
+        );
+    final http.Response response = await http.get(uri);
+    final decodedResponse = json.decode(response.body);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    return formattedAddress;
+  }
+
+  void _getUserLocation() async {
+    final currentLocation = await geoloc.Location().getLocation();
+    final address = await _getAddress(currentLocation.latitude, currentLocation.longitude);
+    _getLocationMarker(address, geocode: false, lat: currentLocation.latitude, lng: currentLocation.longitude);
+  }
+
   void _updateLocation() {
     if(!_addressInputFocusNode.hasFocus) {
-      getLocationMarker(_addressInputController.text);
+      _getLocationMarker(_addressInputController.text);
     }
   }
 
@@ -98,6 +119,10 @@ class _LocationInputState extends State<LocationInput> {
           )
         ),
         SizedBox(height: 10.0),
+        FlatButton(
+          child: Text('Locate User'),
+          onPressed: _getUserLocation,
+        ),
         Container(
           height: 300,
           child: GoogleMap(
