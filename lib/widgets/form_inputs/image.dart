bbc5de5d76a9dcd11d956fc2ef9fc03b22fbda3d
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-06 21:27:56 +0300

    15.3 Adding the use camera & gallery image button

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -8,6 +8,32 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(context: context, builder: (BuildContext context) {
+      return Container(
+        height: 150,
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Text('Pick an Image', style: TextStyle(fontWeight: FontWeight.bold),),
+            SizedBox(height: 5.0),
+            FlatButton(
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+              child: Text('Use camera'),
+            ),
+            FlatButton(
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+              child: Text('Use Gallery'),
+            )
+          ]
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,7 +43,7 @@ class _ImageInputState extends State<ImageInput> {
             color: Theme.of(context).primaryColor,
             width: 1.0
           ),
-          onPressed: () {},
+          onPressed: () => _openImagePicker(context),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
