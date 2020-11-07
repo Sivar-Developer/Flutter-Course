3992e1e90b3dd691b67e3940e82aa8c6824305a1
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-07 16:12:43 +0300

    15.10 Storing data and move it to a temporary storage

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
 const Busboy = require('busboy');
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const fbAdmin = require('firebase-admin');
+const uuid = require('uuid/v4');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,26 +14,34 @@ const fs = require('fs');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const gcconfig = {
+    projectId: 'flutter-products-7ddd6',
+    keyfilename: 'flutter-products.json'
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
+
+fbAdmin.initializeApp({ credential: fbAdmin.credential.cert(require('./flutter-products.json')) })
 
 exports.storeImage = functions.https.onRequest((request, response) => {
     return cors(req, response, () => {
-        if(request.method !== 'POST') {
-            return response.status(500).json({message: 'Not allowed.'});
+        if (request.method !== 'POST') {
+            return response.status(500).json({ message: 'Not allowed.' });
         }
 
-        if(request.headers.authorization || !req.headrs.authorization.startsWith('Bearer ')) {
-            return response.status(401).json({error: 'Unauthorized.'});
+        if (request.headers.authorization || !req.headrs.authorization.startsWith('Bearer ')) {
+            return response.status(401).json({ error: 'Unauthorized.' });
         }
 
         let idToken;
         idToken = req.headers.authorization.split('Bearer ')[1];
 
-        const busboy = new Busboy({headers: req.headers});
+        const busboy = new Busboy({ headers: req.headers });
         let uploadData;
 
         busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
             const filepath = path.join(os.tmpdir(), filename);
-            uploadData = {filepath: filepath, type: mimetype, name: filename};
+            uploadData = { filepath: filepath, type: mimetype, name: filename };
             file.pipe(fs.createWriteStream(filepath));
         });
 
@@ -40,7 +50,38 @@ exports.storeImage = functions.https.onRequest((request, response) => {
         });
 
         busboy.on('finish', () => {
-            //
+            const bucket = gcs.bucket('flutter-products-7ddd6.appspot.com/');
+            const id = uuid();
+            let imagePath = 'images/' + id + '-' + uploadData.name
+            if (oldImagePath) {
+                imagePath = oldImagePath;
+            }
+
+            return fbAdmin
+                .auth()
+                .verifyIdToken(idToken)
+                .then(decodedToken => {
+                    return bucket.upload(uploadData.filepath, {
+                        uploadType: 'media',
+                        destination: imagePath,
+                        metadata: {
+                            metadata: {
+                                contentType: uploadData.type,
+                                firebaseStorageDownloadToken: id
+                            }
+                        }
+                    });
+                })
+                .then(() => {
+                    return response.status(201).json({
+                        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/' + bucket.name + '/o/' + encodeURIComponent(imagePath) + '?alt=media&token=' + id,
+                        imagePath: imagePath
+                    });
+                })
+                .catch(error => {
+                    return response.status(401).json({ error: 'Unauthorized.' });
+                });
         });
+        return busboy.end(request.rawBody);
     });
 });
