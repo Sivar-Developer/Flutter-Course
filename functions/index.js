cd28b996a155783a455a489f3e288f707e3fdec9
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-07 15:33:00 +0300

    15.8 Server side code

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,20 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.storeImage = functions.https.onRequest((request, response) => {
+    return cors(req, response, () => {
+        if(request.method !== 'POST') {
+            return response.status(500).json({message: 'Not allowed.'});
+        }
+
+        if(request.headers.authorization || !req.headrs.authorization.startsWith('Bearer ')) {
+            return response.status(401).json({error: 'Unauthorized.'});
+        }
+
+        let idToken;
+        idToken = req.headers.authorization.split('Bearer ')[1];
+
+        
+    });
+});
