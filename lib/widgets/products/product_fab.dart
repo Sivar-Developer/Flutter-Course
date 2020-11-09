220dafa033a641b3568d1b91379cca38e2703711
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-09 16:01:55 +0300

    16.4 Working on contact button:

--- lib/widgets/products/product_fab.dart
+++ lib/widgets/products/product_fab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course/models/product.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter_course/scoped-models/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProductFab extends StatefulWidget {
   final Product product;
@@ -29,7 +30,14 @@ class _ProductFabState extends State<ProductFab> {
               backgroundColor: Theme.of(context).cardColor,
               heroTag: 'contact',
               mini: true,
-              onPressed: () {},
+              onPressed: () async {
+                final url = 'mailto:${widget.product.userEmail}';
+                if(await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch!';
+                }
+              },
               child: Icon(Icons.mail, color: Theme.of(context).accentColor),
             ),
           ),
